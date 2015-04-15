02a118d84fa57190534cfc126fe1bca6552b3e37
Roadagain <Roadagain@users.noreply.github.com>
2015-04-15 09:53:16 +0900

    argument check was added
    if argument length is equals 0, there are no input files

--- org/roadagain/brainfuck/Main.java
+++ org/roadagain/brainfuck/Main.java
@@ -2,5 +2,8 @@ package org.roadagain.brainfuck;
 
 public class Main {
     public static void main(String[] args){
+        if (args.length == 0){
+            throw new IllegalArgumentException("no input files");
+        }
     }
 }
