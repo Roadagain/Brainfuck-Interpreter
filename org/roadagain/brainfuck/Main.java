3e39bef0f588dc9136e55681be01f847f74c8e69
Roadagain <Roadagain@users.noreply.github.com>
2015-04-15 15:06:08 +0900

    javadoc comment was added

--- org/roadagain/brainfuck/Main.java
+++ org/roadagain/brainfuck/Main.java
@@ -1,6 +1,17 @@
 package org.roadagain.brainfuck;
 
+/**
+ * Main class of Brainfuck-interpreter
+ * @author Roadagain
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * Main method
+     * @param args source file's name (although call with more than one argument,
+     *          this program interprets only the first source file)
+     * @throws IllegalArgumentException if main called with no arguments
+     */
     public static void main(String[] args){
         if (args.length == 0){
             throw new IllegalArgumentException("no input files");
