05dc0041396f03e9252f38879197c5f518558136
Roadagain <Roadagain@users.noreply.github.com>
2015-04-16 21:53:52 +0900

    wipe out IllegalArgumentException
    no reasons to throw IllegalArgumentException

--- src/org/roadagain/brainfuck/Main.java
+++ src/org/roadagain/brainfuck/Main.java
@@ -10,11 +10,11 @@ public class Main {
      * Main method
      * @param args source file's name (although call with more than one argument,
      *          this program interprets only the first source file)
-     * @throws IllegalArgumentException if main called with no arguments
      */
     public static void main(String[] args){
         if (args.length == 0){
-            throw new IllegalArgumentException("no input files");
+            System.out.println("Error: no input files");
+            System.exit(-1);
         }
     }
 }
